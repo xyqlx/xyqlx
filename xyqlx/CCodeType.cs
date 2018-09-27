1ab18fa636b7f3b1c6bcc02501d71cab18fabf1b
xyqlx <bgxmy_b@163.com>
2018-09-27 09:43:17 +0800

    更改使用了xml的方式，祝我好运

---
+++ xyqlx/CCodeType.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Xml;
+
+namespace xyqlx
+{
+    class CCodeType
+    {
+        private bool succeed;
+        private string language;
+        private string typeName;
+        private string callCommand;
+        private string createPath;
+        private class CAnno
+        {
+            private string firstLine;
+            private string beforeLine;
+            private string lastLine;
+
+            public string FirstLine { get => firstLine; set => firstLine = value; }
+            public string BeforeLine { get => beforeLine; set => beforeLine = value; }
+            public string LastLine { get => lastLine; set => lastLine = value; }
+
+        }
+        private CAnno annotation = new CAnno();
+        private class CImport
+        {
+            private string beforeInclude;
+            private string afterInclude;
+            private class CIncludes
+            {
+                private class CInclude
+                {
+                    private string call;
+                    private string text;
+                    private string asText;
+                    private string appText;
+
+                    public string Call { get => call; set => call = value; }
+                    public string Text { get => text; set => text = value; }
+                    public string AsText { get => asText; set => asText = value; }
+                    public string AppText { get => appText; set => appText = value; }
+                }
+                private List<CInclude> includesContent;
+
+                private List<CInclude> IncludesContent { get => includesContent; set => includesContent = value; }
+            }
+            private CIncludes includes;
+
+            public string BeforeInclude { get => beforeInclude; set => beforeInclude = value; }
+            public string AfterInclude { get => afterInclude; set => afterInclude = value; }
+            private CIncludes Includes { get => includes; set => includes = value; }
+        }
+        private CImport import=new CImport();
+        private string context;
+        private string openStyle;
+
+        public CCodeType(string typeName)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("C:\\xy\\xyqlx.xml");
+            XmlElement root = doc.DocumentElement;
+            XmlNode node = root.SelectSingleNode("/xyqlx/languages/language/types/type[/call/text()='" + typeName + "']");
+            if(node == null)
+            {
+                succeed = false;
+                return;
+            }
+            succeed = true;
+            XmlNode langNode = node.ParentNode.ParentNode;
+            SetAnnotation(node.SelectSingleNode(".//annotation/firstLine/text()").Value,
+                node.SelectSingleNode(".//annotation/beforeLine/text()").Value,
+                node.SelectSingleNode(".//annotation/lastLine/text()").Value);
+            CallCommand = typeName;
+            context = node.SelectSingleNode(".//context/text/text()").Value;
+            createPath = node.SelectSingleNode(".//path/text()").Value;
+            SetImport(node.SelectSingleNode(".//import/beforeInclude/text()").Value,
+                node.SelectSingleNode(".//import/afterInclude/text()").Value);
+            language = langNode.SelectSingleNode(".//name/text()").Value;
+            openStyle = node.SelectSingleNode(".//openStyle/text()").Value;
+        }
+
+        public string Language { get => language; set => language = value; }
+        public string TypeName { get => typeName; set => typeName = value; }
+        public string CallCommand { get => callCommand; set => callCommand = value; }
+        public string CreatePath { get => createPath; set => createPath = value; }
+
+        public string GetFirstLine()
+        {
+            return annotation.FirstLine;
+        }
+
+        public string GetBeforeLine()
+        {
+            return annotation.BeforeLine;
+        }
+
+        public string GetLastLine()
+        {
+            return annotation.LastLine;
+        }
+
+        public void SetAnnotation(string firstLine, string beforeLine, string lastLine)
+        {
+            annotation.LastLine = lastLine;
+            annotation.BeforeLine = beforeLine;
+            annotation.FirstLine = firstLine;
+        }
+
+        public string GetBeforeInclude()
+        {
+            return import.BeforeInclude;
+        }
+
+        public string GetAfterInclude()
+        {
+            return import.AfterInclude;
+        }
+
+        public void SetImport(string beforeInclude, string afterInclude)
+        {
+            import.BeforeInclude = beforeInclude;
+            import.AfterInclude = afterInclude;
+        }
+
+        public string Context { get => context; set => context = value; }
+        public string OpenStyle { get => openStyle; set => openStyle = value; }
+        public bool Succeed => succeed;
+    }
+}
--- xyqlx/Program.cs
+++ xyqlx/Program.cs
@@ -6,13 +6,12 @@ namespace xyqlx
 {
     class Program
     {
+        private static Dictionary<string, string> argsDict = new Dictionary<string, string>();
         static void Main(string[] args)
         {
-            Dictionary<string, string> argsDict = new Dictionary<string, string>();
             string lastArg = "";
             //这里没有考虑args可能出现的其它情形，甚至可能报错（
             if (args.Length == 0) return;
-            argsDict.Add(args[0].Substring(1, 2), args[0].Substring(3));
             foreach (string i in args)
             {
                 if (i[0] == '-')
